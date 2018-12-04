25f53c26c0aee01c2b1fc589a544dd9ffd6b7e87
amaclean <andrew.n.maclean@gmail.com>
2018-12-04 13:14:25 -0800

    clean up

---
+++ build/precache-manifest.1b18d9b82db951cb536a62a7fda05156.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "df1606fcc75a37dcdd972802d3682374",
+    "url": "/static/media/toolbox-screenshot.df1606fc.png"
+  },
+  {
+    "revision": "2b96791d95cc8b698d86dc947879228c",
+    "url": "/static/media/the-lesson-box.2b96791d.png"
+  },
+  {
+    "revision": "1831ea7121154bb80cdd36ff924b18e9",
+    "url": "/static/media/devfest.1831ea71.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7bf94f90916ce350b9f8",
+    "url": "/static/js/main.7bf94f90.chunk.js"
+  },
+  {
+    "revision": "dae31ddbe5c1d526b6e8",
+    "url": "/static/js/1.dae31ddb.chunk.js"
+  },
+  {
+    "revision": "7bf94f90916ce350b9f8",
+    "url": "/static/css/main.89968723.chunk.css"
+  },
+  {
+    "revision": "07fcc12b45d09c70443f32cee4f90815",
+    "url": "/index.html"
+  }
+];
